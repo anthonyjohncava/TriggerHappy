ad42b812471b1c186f3e4df378013c3b57c23924
John Andrew Llorera <llojm004@mymail.unisa.edu.au>
2021-06-08 13:44:18 +0930

    update files

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class MyGdxGame extends ApplicationAdapter {
+
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
