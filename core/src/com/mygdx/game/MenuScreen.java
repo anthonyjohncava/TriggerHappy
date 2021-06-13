f2f8fe86026abcaf5ec0104dcb1ea630fd9d8c81
Anthony John Cava <cavay010@mymail.unisa.edu.au>
2021-06-13 16:27:11 +0930

    Added game title on menuscreen

--- core/src/com/mygdx/game/MenuScreen.java
+++ core/src/com/mygdx/game/MenuScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -15,8 +16,11 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 public class MenuScreen implements Screen{
     MyGdxGame game; // Note it’s "MyGdxGame" not "Game"
 
+    private SpriteBatch batch;
     private Skin skin;
     private Stage stage;
+
+    private Texture gameName;
 
     // constructor to keep a reference to the main Game class
     public MenuScreen(MyGdxGame game) {
@@ -26,20 +30,25 @@ public class MenuScreen implements Screen{
 
 
     public void create() {
+        batch = new SpriteBatch();
         Gdx.app.log("MenuScreen: ","menuScreen create");
+
+        gameName = new Texture(Gdx.files.internal("title.png"));
 
         skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
         stage = new Stage();
         final TextButton button = new TextButton("PLAY", skin);
         button.setWidth(300f);
         button.setHeight(100f);
-        button.setPosition(Gdx.graphics.getWidth() /2 - 150f, Gdx.graphics.getHeight()/2 - 0f);
+        button.setPosition(Gdx.graphics.getWidth()/2 - 400f, Gdx.graphics.getHeight()/2 - 170f);
+//        button.setPosition(Gdx.graphics.getWidth() /2 - 150f, Gdx.graphics.getHeight()/2 - 0f);
 
 
         final TextButton exitBtn = new TextButton("EXIT", skin);
         exitBtn.setWidth(300f);
         exitBtn.setHeight(100f);
-        exitBtn.setPosition(Gdx.graphics.getWidth() /2 - 150f, Gdx.graphics.getHeight()/2 - 120f);
+//        exitBtn.setPosition(Gdx.graphics.getWidth() /2 - 150f, Gdx.graphics.getHeight()/2 - 120f);
+        exitBtn.setPosition(Gdx.graphics.getWidth()/2 + 50f, Gdx.graphics.getHeight()/2 - 170f);
 
         button.addListener(new ClickListener()
         {
@@ -72,9 +81,13 @@ public class MenuScreen implements Screen{
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+
+        batch.begin();
+        batch.draw(gameName, Gdx.graphics.getWidth() /2 - 400f, Gdx.graphics.getHeight()/2 - 50f, 700, 200);
+        batch.end();
         stage.draw();
     }
 
